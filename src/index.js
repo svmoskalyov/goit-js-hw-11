88d43f469f7eb6ca6ac6b6be3a26d87d03941362
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-03 19:38:11 +0300

    adds base files

--- src/index.js
+++ src/index.js
@@ -62,19 +62,19 @@ function createMarkupImg(images) {
         comments,
         downloads,
       }) => /*html*/ `<div class="photo-card">
-            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <img class="gallery-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
             <div class="info">
                 <p class="info-item">
-                    <b>Likes:</b> ${likes}
+                    <b>Likes: </b>${likes}
                 </p>
                 <p class="info-item">
-                    <b>Views:</b> ${views}
+                    <b>Views: </b>${views}
                  </p>
                 <p class="info-item">
-                    <b>Comments:</b> ${comments}
+                    <b>Comments: </b>${comments}
                 </p>
                 <p class="info-item">
-                    <b>Downloads:</b> ${downloads}
+                    <b>Downloads: </b>${downloads}
                 </p>
             </div>
         </div>`
