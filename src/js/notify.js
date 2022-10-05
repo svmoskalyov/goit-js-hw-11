8d3373e5fe6163ca993eb6a64617dd7742e2b3eb
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-05 20:41:07 +0300

    Update notify.js

--- src/js/notify.js
+++ src/js/notify.js
@@ -2,12 +2,12 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export function onError(message) {
   Notify.failure(message, {
-    position: 'center-center',
+    position: 'right-top',
   });
 }
 
 export function onSuccess(message) {
   Notify.success(message, {
-    position: 'center-center',
+    position: 'right-top',
   });
 }
